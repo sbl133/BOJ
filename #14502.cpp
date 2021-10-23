33fde09d2bf8cb7695a5d9d9f43b0a87c93e923e
sbl133 <70326426+sbl133@users.noreply.github.com>
2021-10-24 03:34:18 +0900

    [BOJ] #14502 연구소

--- #14502.cpp
+++ #14502.cpp
@@ -1,18 +1,20 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include<cstdio>
+#include<iostream>
 #include<queue>
 #include<cstring>
 using namespace std;
 int n, m;
+// 연구소 상태
 int lab[8][8];
+// 바이러스 퍼짐 여부
 bool visited[8][8];
-int virus;
+// 연구소에 있는 벽의 총 갯수
 int walls;
+// 안전영역 최대크기
 int result;
 int dx[4] = { 0, 0, 1, -1 };
 int dy[4] = { -1, 1, 0, 0 };
-int fillVirus(queue<pair<int, int>>& virusPos) {
-	int ret = 0;
+// 바이러스가 퍼졌을때 연구소에 있는 2의 갯수 구하기
+int fillVirus(queue<pair<int, int>>& virusPos, int virus) {
 	while (!virusPos.empty()) {
 		int x = virusPos.front().first;
 		int y = virusPos.front().second;
@@ -20,54 +22,69 @@ int fillVirus(queue<pair<int, int>>& virusPos) {
 		for (int i = 0; i < 4; i++) {
 			int nextX = x + dx[i];
 			int nextY = y + dy[i];
+			// 연구소를 벗어나면 continue
 			if (nextX < 0 || nextX >= m || nextY < 0 || nextY >= n)continue;
+			// 방문하고자 하는 곳이 0이고 아직 바이러스가 안퍼진 곳이면
 			if (lab[nextY][nextX] == 0 && visited[nextY][nextX] == 0) {
+				// 해당장소 queue에 넣고 visited체크, virus갯수 증가
 				virusPos.push(make_pair(nextX, nextY));
 				visited[nextY][nextX] = true;
-				ret++;
+				virus++;
 			}
 		}
 	}
-	return ret;
+	return virus;
 }
-int calcArea() {
+// 안전 영역 넓이 구하기
+void calcArea() {
 	queue<pair<int, int>> virusPos;
+	int virus = 0;
 	memset(visited, 0, sizeof(visited));
+	// bfs를 위해 바이러스가 있는곳 queue에 넣기
+	// visited, virus 계산
 	for (int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++) 
 			if (lab[i][j] == 2) {
 				virusPos.push(make_pair(j, i));
-				visited[i][j] = true;;
+				visited[i][j] = true;
+				virus++;
 			}
-	int ret = n * m - (walls + 3 + fillVirus(virusPos) + virus);
-	return ret;
+	// 안전영역 = 전체넓이 - (벽의 총 갯수 + virus의 갯수)
+	int ret = n * m - (walls + fillVirus(virusPos, virus));
+	result = max(result, ret);
 }
-int laboratory(int wall, int next) {
-	int area = n * m;
-	if (next >= area) return -1;
-	if (wall == 0) return calcArea();
-	int x = next % m;
-	int y = next / m;
-	if (lab[y][x] == 0) {
-		lab[y][x] = 1;
-		result = max(result, laboratory(wall - 1, next + 1));
-		lab[y][x] = 0;
+// 벽 설치
+void constructWall(int wall, int here) {
+	// 벽 설치 다했으면 안전영역 계산
+	if (wall == 0) {
+		calcArea();
+		return;
 	}
-	result = max(result, laboratory(wall, next + 1));
-	return result;
+	int area = n * m;
+	// 더 이상 벽을 설치할 곳이 없는 경우 return
+	if (here >= area) return;
+	int x = here % m;
+	int y = here / m;
+	// 현재 영역이 0이면 벽을 설치 하는 경우
+	if (lab[y][x] == 0) {
+		// 벽 설치하고 다음 영역으로 넘어감
+		lab[y][x] = 1; walls++;
+		constructWall(wall - 1, here + 1);
+		// 원상복구
+		lab[y][x] = 0; walls--;
+	}
+	// 현재 영역은 벽을 설치 안하고 다음 영역으로 넘어가는 경우
+	constructWall(wall, here + 1);
 }
-
 int main() {
-	scanf("%d %d", &n, &m);
-	virus = 0;
-	result = 0;
+	cin >> n >> m;
+	walls = result = 0;
 	for(int i = 0; i < n; i++)
 		for (int j = 0; j < m; j++) {
-			scanf("%d", &lab[i][j]);
+			cin >> lab[i][j];
 			if (lab[i][j] == 1)
-				walls++;
-			else if (lab[i][j] == 2) 
-				virus++;	
+				walls++;	
 		}
-	printf("%d\n", laboratory(3, 0));
+	constructWall(3, 0);
+	cout << result << endl;
 }
